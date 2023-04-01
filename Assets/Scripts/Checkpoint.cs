b42952c8c1dfe9af7edd062b3ff35c1f4be25f84
Yonah <boleyonah@gmail.com>
2023-04-01 10:54:30 +0200

    First project upload

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public SpriteRenderer theSR;
+
+    public Sprite checkpointOn, checkpointOff;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.CompareTag("Player")) // also works with other.tag == "Player"
+        { 
+            CheckpointController.instance.DeactivateCheckpoints();
+
+            theSR.sprite = checkpointOn;
+
+            CheckpointController.instance.SetSpawnPoint(transform.position); // Set players to spawn at latest checkpoint
+        }
+    }
+
+    public void ResetCheckpoint() // Set the last checkpoint off when a new one is turned on
+    {
+        theSR.sprite = checkpointOff;
+    }
+}
